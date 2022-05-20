4a5b4bfb5bfb873de10f183352811c31a969cc02
Maicol <michael.negrete16@gmail.com>
2022-05-20 09:01:10 -0500

    Mantener el estado de la autenticacion al recargar

--- src/Routers/AppRouter.jsx
+++ src/Routers/AppRouter.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import {
     BrowserRouter,
     Routes,
@@ -5,23 +6,40 @@ import {
     Navigate
   } from "react-router-dom";
 
+import { useDispatch } from "react-redux"; 
+
 import JournalScreen from "../components/journal/JournalScreen";
 import AuthRouter from "./AuthRouter";
 
-const AppRouter = () => {
-  return (
-    <BrowserRouter>
-        <Routes>
-            {/* El * al final de la ruta indica que tiene hijos para poder encadenarlos */}
-            <Route path="/auth/*" element={<AuthRouter/>}/>
-            <Route exact path="/" element={<JournalScreen/>} />
+import {firebase} from "../firebase/firebase-config";
+import { login } from "../actions/auth";
 
-            {/* Si ninguna ruta es valida caera aqui */}
-            <Route path="*" element={<Navigate replace to='/auth'/>} />
-            
-        </Routes>
-    </BrowserRouter>
-  )
+const AppRouter = () => {
+
+    const dispatch = useDispatch()
+
+    // Saber si ya esta autenticado o no el usuario
+    useEffect(() => {
+       firebase.auth().onAuthStateChanged(user => {
+          if(user?.uid){
+              dispatch(login(user.uid, user.displayName))
+          }
+      })
+    },[dispatch])
+
+    return (
+      <BrowserRouter>
+          <Routes>
+              {/* El * al final de la ruta indica que tiene hijos para poder encadenarlos */}
+              <Route path="/auth/*" element={<AuthRouter/>}/>
+              <Route exact path="/" element={<JournalScreen/>} />
+
+              {/* Si ninguna ruta es valida caera aqui */}
+              <Route path="*" element={<Navigate replace to='/auth'/>} />
+              
+          </Routes>
+      </BrowserRouter>
+    )
 }
 
 export default AppRouter
